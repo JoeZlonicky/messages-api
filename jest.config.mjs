ed3288d3ee270fd21d155d1bba0378aa35e43283
JoeZlonicky <joe.zlonicky@gmail.com>
2024-09-06 16:11:08 -0700

    Remove unnecessary key/value from jest config

--- jest.config.mjs
+++ jest.config.mjs
@@ -6,7 +6,4 @@ const defaultEsmPreset = createDefaultEsmPreset();
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
   ...defaultEsmPreset,
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
 };
