db833a180d272b34ab69d766522bea1df4c44c16
JoeZlonicky <joe.zlonicky@gmail.com>
2024-09-26 15:48:27 -0700

    Update index tests

--- src/routes/Index.test.ts
+++ src/routes/Index.test.ts
@@ -3,9 +3,9 @@ import { test } from '@jest/globals';
 import request from 'supertest';
 
 test('index route', (done) => {
-  request(app)
-    .get('/')
-    .expect('Content-Type', /json/)
-    .expect({ Hello: 'World' })
-    .expect(200, done);
+  request(app).get('/').expect({ Hello: 'World' }).expect(200, done);
+});
+
+test('page not found', (done) => {
+  request(app).get('/invalid-page-route').expect(404, done);
 });
